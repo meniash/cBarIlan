84d07c990150492b3e0211713423ef13eb10f8b0
meniash <meni.ashurov@gmail.com>
2025-09-11 10:37:32 +0300

    finished ex1

--- ex1/main.c
+++ ex1/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
-void main() {
-    printf("hello for ex1\n");
+//print hello world program and check that everything is working in IDE and no warning pop up
+int main() {
+printf("Hello, World!\n");
+return 0;
 }
