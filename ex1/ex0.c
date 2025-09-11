5841160f5bf8a41f73dc314cc8bdca52cbbecc44
meniash <meni.ashurov@gmail.com>
2025-09-11 15:22:17 +0300

    now can pass args to program

--- ex1/ex0.c
+++ ex1/ex0.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 //print hello world program and check that everything is working in IDE and no warning pop up
-int main() {
+int main(int argc, char* argv[]) {
 printf("Hello, World!\n");
+printf("Program name: %s\n", argv[0]);
+for (int i = 1; i < argc; i++) {
+    printf("Arg %d: %s\n", i, argv[i]);
+}
 return 0;
 }
